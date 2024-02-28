e37e84c6715385b5822ea59d8293dd545feb44a7
K.Shajahan <K.Shajahan@Shajahan-2000>
2024-02-28 22:18:37 +0530

    full code of paypal integration

---
+++ Paypal-Integration/src/test/java/com/sampleproject/PaypalIntegration/PaypalIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sampleproject.PaypalIntegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaypalIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
